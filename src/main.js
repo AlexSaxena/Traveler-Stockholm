9d3ff04e918378716079958ae1dd1096a0ec75b8
Alex Saxena <saxenaalex@gmail.com>
2024-11-30 16:14:04 +0100

    Adds functionality for changing display hidden on nav-menu

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import "./style.css";
 
 console.log("General Kenobi!");
+
+document.getElementById("menu-button").addEventListener("click", () => {
+  const menu = document.getElementById("nav-menu");
+  menu.classList.toggle("hidden");
+});
