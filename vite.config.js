9581240bb0b586dc86bf5e4dd4a21b95b98d6b40
Alex <saxenaalex@gmail.com>
2024-12-02 09:46:22 +0100

    Adds name of project as base Url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "repo-name",
+  base: "Traveler-Stockholm",
   build: {
     rollupOptions: {
       input: {
